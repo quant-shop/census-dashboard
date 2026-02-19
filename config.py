4c7c05774e934112bcd5ebac59a806f568266a66
jibekgupta <jibek.gupta@bison.howard.edu>
2026-02-19 01:55:38 -0500

    categorized dropdown

--- config.py
+++ config.py
@@ -6,7 +6,27 @@ CENSUS_VARIABLES = {
     "B03002_003E": {"label": "White Alone, Not Hispanic", "category": "Demographics", "format": ","},
     "B03002_004E": {"label": "Black or African American Alone", "category": "Demographics", "format": ","},
     "B03002_012E": {"label": "Hispanic or Latino", "category": "Demographics", "format": ","},
+
+    # we will add more variables later
 }
 
 VARIABLE_CODES = list(CENSUS_VARIABLES.keys())
-print(VARIABLE_CODES)
+
+
+VARIABLE_OPTIONS =[]
+seen_categories = set()
+
+for code, info in CENSUS_VARIABLES.items():
+    category = info['category']
+    if category not in seen_categories:
+        seen_categories.add(category)
+        VARIABLE_OPTIONS.append({
+            "label": f"{category}",
+            "value": f"_header_{category}",
+            "disabled": True
+        })
+    
+    VARIABLE_OPTIONS.append({
+        "label": f"{    info['label']} ({code})",
+        "value": code
+    })
